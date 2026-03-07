c22acdff54d139ec51e68b604bae3a83d43c3330
DiegoRodrigues06 <diegodash06@gmail.com>
2026-03-07 17:22:11 -0300

    react framework

--- game-launcher-app/public/electron.js
+++ game-launcher-app/public/electron.js
@@ -6,10 +6,18 @@ const createWindow = () => {
 // carrega a página (html) em uma nova instance do BrowserWindow
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
   })
 
-  win.loadFile('index.html')
+//tenta carregar a página acessando a url do host do frontend,
+//se não conseguir tenta denovo apos 1 segundo
+  win.loadURL('http://localhost:5173/').catch(() => {
+    setTimeout(() => win.loadURL('http://localhost:5173/'), 1000)
+  })
 }
 
 app.whenReady().then(() => {
---
+++ game-launcher-app/src/pages/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+        <h1>Game Launcher</h1>
+    </>
+  )
+}
+
+export default App
