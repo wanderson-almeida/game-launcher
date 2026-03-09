4da0fb1a8c7b13f8f4f00a80a2fbf6f6b5322538
Wesley Nogueira <wessleynog@gmail.com>
2026-03-09 15:54:17 -0300

    fix: implementação de screenshots do jogo

--- game-launcher-app/src/backend/api_rawg_requests.py
+++ game-launcher-app/src/backend/api_rawg_requests.py
@@ -12,21 +12,25 @@ async def GetGameDetails(gameName):
         r = await client.get(f"/games/{gameName}?key={apiKey}")
         data = r.json()
 
+        screenshots = await client.get(f"/games/{gameName}/screenshots?key={apiKey}")
+        images = screenshots.json()
+
         organized = {
             "id": data["id"],
             "name_original": data["name_original"],
-            "description": data["description"],
             "released": data["released"],
             "updated": data["updated"],
-            "background_image": data["background_image"],
-            "background_image_additional": data["background_image_additional"],
-            "playtime": data["playtime"],
             "achievements_count": data["achievements_count"],
             "rating": data["rating"],
-            "ratings_count": data["ratings_count"]
+            "ratings_count": data["ratings_count"],
+            "playtime": data["playtime"],
+            "description": data["description"],
+            "background_image": data["background_image"],
+            "background_image_additional": data["background_image_additional"],
+            "screenshots": [screenshot["image"] for screenshot in images["results"]]
         }
 
         print(json.dumps(organized, indent=4))
 
 
-asyncio.run(GetGameDetails("raft"))
+asyncio.run(GetGameDetails("hytale"))
