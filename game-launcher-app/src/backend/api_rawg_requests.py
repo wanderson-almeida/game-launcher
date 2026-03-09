90fa9f469ad1d0e107de38d7c7362b3ec9eadf00
Wesley Nogueira <wessleynog@gmail.com>
2026-03-09 15:26:05 -0300

    feat: puxando informações especificas do jogo

--- game-launcher-app/src/backend/api_rawg_requests.py
+++ game-launcher-app/src/backend/api_rawg_requests.py
@@ -1,13 +1,32 @@
-import httpx, asyncio, json
+import httpx, asyncio, json, os
+from dotenv import load_dotenv
 
-apiKey = "2e457f28ca4d42d086a5f6e936b07d5f"
-baseGamesUrl = f"https://api.rawg.io/api"
+load_dotenv(verbose=True)
+
+baseGamesUrl = os.getenv("BASE_GAMES_URL")
+apiKey = os.getenv("RAWG_API_KEY")
+
 
 async def GetGameDetails(gameName):
     async with httpx.AsyncClient(base_url=baseGamesUrl) as client:
         r = await client.get(f"/games/{gameName}?key={apiKey}")
         data = r.json()
 
-        print(json.dumps(data, indent=4))
+        organized = {
+            "id": data["id"],
+            "name_original": data["name_original"],
+            "description": data["description"],
+            "released": data["released"],
+            "updated": data["updated"],
+            "background_image": data["background_image"],
+            "background_image_additional": data["background_image_additional"],
+            "playtime": data["playtime"],
+            "achievements_count": data["achievements_count"],
+            "rating": data["rating"],
+            "ratings_count": data["ratings_count"]
+        }
+
+        print(json.dumps(organized, indent=4))
+
 
 asyncio.run(GetGameDetails("raft"))
