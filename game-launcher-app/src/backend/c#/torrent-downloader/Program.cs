e2b7ed193d1c36f470b5ded5ac436e25a363dd66
Wesley Nogueira <wessleynog@gmail.com>
2026-03-10 20:00:36 -0300

    feat: criação do torrent-downloader.exe

--- game-launcher-app/src/backend/c#/torrent-downloader/Program.cs
+++ game-launcher-app/src/backend/c#/torrent-downloader/Program.cs
@@ -1,10 +1,13 @@
 ﻿using MonoTorrent;
 using MonoTorrent.Client;
 
-var engine = new ClientEngine();
-var magnet = MagnetLink.Parse("magnet:?xt=urn:btih:65dc7c504e6e83de5a3c8db79283bfe64e50155c&dn=Fraymakers&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337");
+string magnet = args[0];
 
-var manager = await engine.AddAsync(magnet, "C:\\Games\\Download");
+var engine = new ClientEngine();
+
+var magnetLink = MagnetLink.Parse(magnet);
+
+var manager = await engine.AddAsync(magnetLink, "C:\\Games\\Download");
 
 await manager.StartAsync();
 
