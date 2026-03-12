77c8384602c3a94d2be4b32443edfa540366e166
Wesley Nogueira <wessleynog@gmail.com>
2026-03-12 17:44:44 -0300

    feat: Algumas coisas mudaram ai

--- game-launcher-app/src/backend/c#/torrent-downloader/obj/Debug/net9.0/torrent-downloader.AssemblyInfo.cs
+++ game-launcher-app/src/backend/c#/torrent-downloader/obj/Debug/net9.0/torrent-downloader.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("torrent-downloader")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab0bfbf198f3f7d48d1826271a65a8b2f66c8a4a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7bb18e501ce0794a047ecd78a98199a9f0765f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("torrent-downloader")]
 [assembly: System.Reflection.AssemblyTitleAttribute("torrent-downloader")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
