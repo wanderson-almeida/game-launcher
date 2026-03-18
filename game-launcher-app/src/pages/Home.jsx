7d18b1ce23e87537907eb177232887e8773510a9
Apolosimao <apolosimao1@gmail.com>
2026-03-18 16:47:25 -0300

    arrumando paginas e logo

--- game-launcher-app/src/pages/Home.jsx
+++ game-launcher-app/src/pages/Home.jsx
@@ -3,7 +3,7 @@ function Home() {
     <div className="home-container">
       <div className="home-box">
         <h1>Bem-vindo 👋</h1>
-        <p>Este launcher é um projeto entre amigos</p>
+        <p>Este launcher é um projeto entre amigos.</p>
       </div>
     </div>
   );
